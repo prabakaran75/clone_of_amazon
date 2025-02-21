033790baaad505dc6eb5f20e45eeae7e7c0abc07
prabakaran75 <62014581+prabakaran75@users.noreply.github.com>
2025-02-21 18:08:42 +0530

    Created deal screen

---
+++ lib/features/home/screens/category_deal_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryDealScreen extends StatefulWidget {
+const CategoryDealScreen({super.key});
+
+@override
+State<CategoryDealScreen> createState() => _CategoryDealScreenState();
+}
+
+class _CategoryDealScreenState extends State<CategoryDealScreen> {
+@override
+Widget build(BuildContext context) {
+    return const Placeholder();
+}
+}
